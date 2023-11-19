2dcc03b9548e65b0ce5321fceb26124a5fd2dd6a
Alex Kuczala <kookzala@gmail.com>
2023-11-18 19:17:35 -0500

    better node labels

--- src/ParseGraph.hs
+++ src/ParseGraph.hs
@@ -1,8 +1,14 @@
 {-# LANGUAGE GADTs #-}
 module ParseGraph (translateGraph) where
 import Conal
+import Data.List (nub)
 
-data NodeData = NodeData {name :: String, inPorts :: [Port], outPorts :: [Port]}
+data NodeData = NodeData {
+    nodeId :: Int,
+    nodeLabel :: String,
+    inPorts :: [Port],
+    outPorts :: [Port]
+    }
     deriving Show
 
 parsePorts :: Ports a -> [Port]
@@ -13,31 +19,40 @@ parsePorts (PairP ps1 ps2) = parsePorts ps1 ++ parsePorts ps2
 
 parseNode :: Int -> Node -> NodeData
 parseNode i (Node name pIn pOut) = NodeData {
-    name = name ++ "_" ++ show i,
+    nodeId = i,
+    nodeLabel = name,
     inPorts = parsePorts pIn,
     outPorts = parsePorts pOut
     }
 
 allPorts :: [NodeData] -> [Port]
-allPorts = concatMap inPorts -- assume all edges are connected to one in and one out port
+allPorts nodes = nub $ concatMap inPorts nodes
 
 data NodePair = NodePair {fromNode :: Maybe NodeData, toNode :: Maybe NodeData}
     deriving Show
 
 findPortNodePair :: Port -> [NodeData] -> (Maybe NodeData, Maybe NodeData)
 findPortNodePair port = foldl go (Nothing, Nothing) where
-    go pair node@(NodeData _ inPorts outPorts) = case (outPorts, inPorts) of
+    go pair node@(NodeData _ _ inPorts outPorts) = case (outPorts, inPorts) of
         _ | port `elem` outPorts -> (Just node, snd pair)
         _ | port `elem` inPorts -> (fst pair, Just node)
         _ -> pair
 
+nodeIdToString :: Int -> String 
+nodeIdToString i = "n" ++ show i
+
+nodeString :: NodeData -> String
+nodeString node = (nodeIdToString . nodeId) node ++ " [label=" ++ nodeLabel node ++ "]"
+
 serializePair :: (Maybe NodeData, Maybe NodeData) -> String
-serializePair (Just n1, Just n2) = name n1 ++ " -> " ++ name n2
+serializePair (Just n1, Just n2) = nodeIdToString (nodeId n1) ++ " -> " ++ nodeIdToString ( nodeId n2)
 serializePair _ = "INVALID"
 
 translateGraph :: [Node] -> [String]
-translateGraph inNodes = map (serializePair . flip findPortNodePair nodes) ports
+translateGraph inNodes = nodeStrings ++ edgeStrings
     where
         nodes = zipWith parseNode [0..] inNodes
         ports = allPorts nodes
+        nodeStrings = map nodeString nodes
+        edgeStrings = map (serializePair . flip findPortNodePair nodes) ports
 
