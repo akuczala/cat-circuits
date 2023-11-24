c6b9486cb406083e3b3444822d4ddc6fdd062235
Alex Kuczala <kookzala@gmail.com>
2023-11-24 01:01:50 -0500

    cleanup examples

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,12 @@
 module Main (main) where
 
-import ParseGraph (translateGraph)
-import Examples (exampleOut, test8, test9, test10, test11, test12, test13)
-import Control.Category ((>>>))
-import GraphVizUtils (buildDotGraph, serializeDotGraph)
-import Graph (Graph)
-import Data.Function ((&))
+import Examples (writeDotExample, examples)
+import Control.Monad (zipWithM_)
 
-writeDotExample :: String -> Graph () () -> IO ()
-writeDotExample fileName example = example & (
-    exampleOut
-    >>> translateGraph
-    >>> uncurry buildDotGraph
-    >>> serializeDotGraph
-    >>> writeFile fileName
-    )
+fileNames :: [String]
+fileNames = map (\i -> "graphs/example" ++ show i ++ ".gv") [0..]
+
 main :: IO ()
 main = do
-    -- putStr $ unlines $ map show (fst $ translateGraph $ exampleOut test8)
-    -- putStrLn ""
-    --putStr $ unlines  (serializeGraph $ exampleOut test8)
-    writeDotExample "graphs/test8.gv" test8
-    writeDotExample "graphs/test9.gv" test9
-    writeDotExample "graphs/test10.gv" test10
-    writeDotExample "graphs/test11.gv" test11
-    writeDotExample "graphs/test12.gv" test12
-    writeDotExample "graphs/test13.gv" test13
-    -- writeFile "graphs/test6.gv" . serializeDotGraph . uncurry buildDotGraph . translateGraph . exampleOut $ test8
+    zipWithM_ writeDotExample fileNames examples
     
--- src/Examples.hs
+++ src/Examples.hs
@@ -1,6 +1,6 @@
 module Examples(
-  exampleOut,
-  test8, test9, test10, test11, test12, test13
+  writeDotExample,
+  examples
 ) where
 import Control.Category.Cartesian
 import Graph
@@ -9,49 +9,54 @@ import qualified Control.Category as C
 import Control.Category.Monoidal
 import Circuits
 import BoolCat (BoolCat(xorC))
-import VecCat (VecCat(toPair, fromPair, splitHead))
+import ParseGraph (translateGraph)
+import GraphVizUtils (buildDotGraph, serializeDotGraph)
+import Data.Function ((&))
 
-test8 :: Graph () ()
-test8 = copy
+examples :: [Graph () ()]
+examples = [
+  pairInput "x" "y"
+  >>> xorC
+  >>> terminalNode "out",
+
+  pairInput "x" "y"
+  >>> halfAdder
+  >>> terminalNode "sum" *** terminalNode "carry"
+  >>> consume,
+
+  copy
+  >>> initialNode "cin" *** pairInput "x" "y"
+  >>> fullAdder (genNode "HADD")
+  >>> terminalNode "s" *** terminalNode "cout"
+  >>> consume,
+
+  copy
   >>> initialNode "cin" *** pairInput "x" "y"
   >>> fullAdder halfAdder
   >>> terminalNode "s" *** terminalNode "cout"
-  >>> consume
+  >>> consume,
 
-test9 :: Graph () ()
-test9 = copy
-  >>> initialNode "cin" *** pairInput "x" "y"
-  >>> fullAdder (genNode "HADD")
-  >>> terminalNode "s" *** terminalNode "cout"
-  >>> consume
-
-test10 :: Graph () ()
-test10 = pairInput "x" "y" >>> xorC >>> terminalNode "out"
-
-test11 :: Graph () ()
-test11 = initialNode "c_x_y"
-  >>> splitHead
-  >>> C.id *** toPair
-  >>> fullAdder (genNode "HADD")
-  >>> fromPair
-  >>> terminalNode "s_c"
-
-test12 :: Graph () ()
-test12 =
   copy
   >>> initialNode "cin" *** pairInput "x" "y"
   >>> twoBitAdder (genNode "FADD")
   >>> terminalNode "s" *** terminalNode "cout"
-  >>> consume
+  >>> consume,
 
-test13 :: Graph () ()
-test13 =
   copy
   >>> initialNode "cin" *** pairInput "x" "y"
   >>> twoBitAdder (fullAdder halfAdder)
   >>> terminalNode "s" *** terminalNode "cout"
   >>> consume
-
+  ]
 
 exampleOut :: Graph () () -> [Node]
 exampleOut graph = snd C.. snd $ runGraph graph UnitP
+
+writeDotExample :: String -> Graph () () -> IO ()
+writeDotExample fileName example = example & (
+    exampleOut
+    >>> translateGraph
+    >>> uncurry buildDotGraph
+    >>> serializeDotGraph
+    >>> writeFile fileName
+    )
