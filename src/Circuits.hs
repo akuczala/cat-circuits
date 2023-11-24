0874ea9cdef6d8fc18c0ff781596ff40872b2093
Alex Kuczala <kookzala@gmail.com>
2023-11-24 13:16:35 -0500

    int / vec bool conversion; more tests

--- src/Circuits.hs
+++ src/Circuits.hs
@@ -1,5 +1,9 @@
 {-# LANGUAGE DataKinds #-}
 module Circuits(
+  boolVecToIntLilEnd,
+  boolVecToIntBigEnd,
+  intToBoolVecBigEnd,
+  intToBoolVecLilEnd,
   halfAdder,
   fullAdder,
   twoBitAdder
@@ -12,6 +16,27 @@ import Control.Category ((>>>))
 import Control.Category.Cartesian
 import VecCat (VecCat (..))
 import qualified Data.Vector.Sized as V
+import GHC.TypeNats
+import Data.Bits (testBit)
+
+boolVecToInt :: (KnownNat n) => (Int -> Int) -> V.Vector n Bool -> Int
+boolVecToInt indexer bv = V.sum (V.zipWith term (V.map indexer $ V.enumFromN 0) bv) where
+  term :: Int -> Bool -> Int
+  term n b = fromEnum b * 2^n
+
+-- convert a boolean vector to an integer, assuming little endian format
+boolVecToIntLilEnd :: (KnownNat n) => V.Vector n Bool -> Int
+boolVecToIntLilEnd = boolVecToInt id
+
+-- convert a boolean vector to an integer, assuming big endian format
+boolVecToIntBigEnd :: (KnownNat n) => V.Vector n Bool -> Int
+boolVecToIntBigEnd bv = boolVecToInt (\i -> V.length bv - i - 1) bv
+
+intToBoolVecLilEnd :: (KnownNat n) => Int -> V.Vector n Bool
+intToBoolVecLilEnd i = V.map (testBit i) (V.enumFromN 0)
+
+intToBoolVecBigEnd ::(KnownNat n) => Int -> V.Vector n Bool
+intToBoolVecBigEnd i = V.reverse $ intToBoolVecLilEnd i
 
 -- (x, y) `k` (sum, carry)
 halfAdder :: BoolCat k => k (Pair Bool) (Pair Bool)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,63 @@
 import qualified System.Exit as Exit
 import Test.HUnit
-import Circuits (halfAdder)
+import qualified Data.Vector.Sized as V
+import Circuits
+import Control.Category ((>>>))
+import Control.Category.Monoidal (MonoidalProduct(..))
+
+testTrue :: Bool -> Test
+testTrue result = TestCase (assertBool "fail" result)
+
+funEqTable :: Eq b => (a -> b) -> [(a, b)] -> Bool
+funEqTable f = all (\(input, output) -> f input == output)
+
+testBoolVecToIntBigEnd :: Test
+testBoolVecToIntBigEnd = testTrue $ funEqTable boolVecToIntBigEnd table1 && funEqTable boolVecToIntBigEnd table2 where
+    table1 = [
+        (V.fromTuple (True, False, False), 4),
+        (V.fromTuple (False, True, True), 3)
+        ]
+    table2 = [
+        (V.fromTuple (True, False, True, False, True), 21),
+        (V.fromTuple (False, True, True, False, False), 12)
+        ]
+
+testBoolVecToIntLilEnd :: Test
+testBoolVecToIntLilEnd = testTrue $ funEqTable boolVecToIntLilEnd table1 && funEqTable boolVecToIntLilEnd table2 where
+    table1 = [
+        (V.fromTuple (True, False, False), 1),
+        (V.fromTuple (False, True, True), 6)
+        ]
+    table2 = [
+        (V.fromTuple (True, False, True, False, True), 21),
+        (V.fromTuple (False, True, True, False, False), 6)
+        ]
+
+testIntToBigEnd :: Test
+testIntToBigEnd = testTrue $ funEqTable intToBoolVecBigEnd table1 && funEqTable intToBoolVecBigEnd table2  where
+    table1 = [
+        (5, V.fromTuple (True, False, True)),
+        (9, V.fromTuple (False, False, True))
+        ]
+    table2 = [
+        (11, V.fromTuple (True, False, True, True)),
+        (7, V.fromTuple (False, True, True, True))
+        ]
+
+testIntToLilEnd :: Test
+testIntToLilEnd = testTrue $ funEqTable intToBoolVecLilEnd table1 && funEqTable intToBoolVecLilEnd table2  where
+    table1 = [
+        (5, V.fromTuple (True, False, True)),
+        (3, V.fromTuple (True, True, False))
+        ]
+    table2 = [
+        (12, V.fromTuple (False, False, True, True)),
+        (20, V.fromTuple (False, False, True, False))
+        ]
 
 
 testHalfAdder :: Test
-testHalfAdder = TestCase (assertBool "fail" result) where
+testHalfAdder = testTrue result where
     result :: Bool
     result = all (\pair -> halfAdder pair == halfAdderTable pair) [(x, y) | x <- [False, True], y <- [False, True]]
     halfAdderTable (x, y) = case (x, y) of
@@ -13,8 +66,49 @@ testHalfAdder = TestCase (assertBool "fail" result) where
         (True, False) -> (True, False)
         (True, True) -> (False, True)
 
+testFullAdder :: Test
+testFullAdder = testTrue result where
+    result :: Bool
+    result = all (\input -> fullAdder halfAdder input == fullAdderTable input) [(cin, (x, y)) | cin <- [False, True], x <- [False, True], y <- [False, True]]
+    fullAdderTable (cin, (x, y)) = case (cin, x, y) of
+        -- (cin, x, y) -> (sum, cout)
+        (False, False, False) -> (False, False)
+        (False, False, True) -> (True, False)
+        (False, True, False) -> (True, False)
+        (False, True, True) -> (False, True)
+        (True, False, False) -> (True, False)
+        (True, False, True) -> (False, True)
+        (True, True, False) -> (False, True)
+        (True, True, True) -> (True, True)
+    
+testTwoBitAdder :: Test
+testTwoBitAdder = testTrue $ funEqTable testFun table where
+    testFun :: (Bool, (Int, Int)) -> (Int, Bool)
+    testFun =
+        second' (intToBoolVecLilEnd *** intToBoolVecLilEnd)
+        >>> twoBitAdder (fullAdder halfAdder)
+        >>> first' boolVecToIntLilEnd
+    table = [
+        (
+            (cin, (x, y)),
+            (
+                mod (x + y + fromEnum cin) 4 ,
+                (x + y + fromEnum cin) `div` 4 == 1
+            )
+        )
+            | cin <- [False, True], x <- [0, 1, 2], y <- [0, 1, 2]
+        ]
+
 tests :: Test
-tests = TestList [TestLabel "halfAdder" testHalfAdder]
+tests = TestList [
+    TestLabel "boolVecToIntBigEnd" testBoolVecToIntBigEnd,
+    TestLabel "boolVecToIntLilEnd" testBoolVecToIntLilEnd,
+    TestLabel "testIntToBigEnd" testIntToBigEnd,
+    TestLabel "testIntToLilEnd" testIntToLilEnd,
+    TestLabel "halfAdder" testHalfAdder,
+    TestLabel "fullAdder" testFullAdder,
+    TestLabel "twoBitAdder" testTwoBitAdder
+    ]
 main :: IO ()
 main = do
     result <- runTestTT tests
