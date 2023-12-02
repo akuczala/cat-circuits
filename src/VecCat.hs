1592178145b5cdd2e352b68b9930bb56ea5477b3
Alex Kuczala <kookzala@gmail.com>
2023-12-02 14:51:38 -0500

    try to simplify defs

--- src/VecCat.hs
+++ src/VecCat.hs
@@ -33,7 +33,7 @@ class Category k => VecCat k where
     postScanl :: (b, a) `k` b -> (b, V.Vector n a) `k` V.Vector n b
     splitScan :: (b, a) `k` (b, c) -> (b, V.Vector n a) `k` (V.Vector n c, b)
     reverse :: V.Vector n a `k` V.Vector n a
-    
+
 instance VecCat (->) where
     index = flip V.index
     splitHead :: V.Vector (1 + n) a -> (a, V.Vector n a)
@@ -53,29 +53,34 @@ instance VecCat (->) where
         extract bcv = (fmap (fromJust Prelude.. snd) bcv, V.last (V.cons b0 (fmap fst bcv)))
     reverse = V.reverse
 
+
+liftVec :: (V.Vector n (Ports a) -> Ports b) -> Graph (V.Vector n a) b
+liftVec f = Graph $ \(VecP v) -> return (f v)
+
 instance VecCat Graph where
-    index i = Graph(\(VecP v) -> return $ V.index v i )
-    splitHead = Graph (\(VecP v) -> f v) where
-        f v = return (PairP x (VecP y))
+    index i = liftVec (`V.index` i)
+    splitHead = liftVec f where
+        f v = PairP x (VecP y)
             where (x, y) = splitHead v
     mergeHead = Graph (\(PairP x (VecP xs)) -> return $ VecP (V.cons x xs))
-    flatOne = Graph(\(VecP v) -> return $ flatOne v)
-    vanishNone = Graph(\(VecP _) -> return UnitP)
-    fromPair = Graph(\(PairP x y) -> return $ VecP $ fromPair (x, y))
-    toPair = Graph(\(VecP v) -> return $ uncurry PairP $ toPair v)
-    zip = Graph(\(PairP (VecP v1) (VecP v2)) -> return $ VecP $ V.zipWith PairP v1 v2)
-    map (Graph f) = Graph(\(VecP v) -> go v) where
+    flatOne = liftVec flatOne
+    vanishNone :: Graph (V.Vector 0 a) ()
+    vanishNone = liftVec (const UnitP)
+    fromPair = Graph (\(PairP x y) -> return $ VecP $ fromPair (x, y))
+    toPair = Graph (\(VecP v) -> return $ uncurry PairP $ toPair v)
+    zip = Graph (\(PairP (VecP v1) (VecP v2)) -> return $ VecP $ V.zipWith PairP v1 v2)
+    map (Graph f) = Graph (\(VecP v) -> go v) where
         go v = do
             u <- V.mapM f v
             return $ VecP u
-    foldl (Graph f) = Graph(\(PairP pb (VecP v)) -> V.foldM (curry go) pb v) where
+    foldl (Graph f) = Graph (\(PairP pb (VecP v)) -> V.foldM (curry go) pb v) where
         go (b,a) = f (PairP b a)
-    postScanl (Graph f) = Graph(\(PairP pb (VecP v)) -> VecP <$> postScanlVecM go pb v) where
+    postScanl (Graph f) = Graph (\(PairP pb (VecP v)) -> VecP <$> postScanlVecM go pb v) where
         go b a = f (PairP b a)
-    splitScan (Graph f) = Graph(\(PairP b0 (VecP as)) -> extract <$> splitScanlVecM go b0 as) where
+    splitScan (Graph f) = Graph (\(PairP b0 (VecP as)) -> extract <$> splitScanlVecM go b0 as) where
         go b a = do
             x <- f (PairP b a)
             return $ case x of
                 PairP y z -> (y, z)
         extract (cs, b) = PairP (VecP cs) b
-    reverse = Graph(\(VecP v) -> return Prelude.. VecP Prelude.. V.reverse $ v)
+    reverse = liftVec (VecP Prelude.. VecCat.reverse)
